4a34a9c591f9c4335d3aed73feb92b1b83645c96
paxnamra <paxnamra@gmail.com>
2023-06-18 16:28:46 +0200

    added logging, set logging in notes service

--- yohoard/backend/src/main/java/com/yohoard/notes/NoteService.java
+++ yohoard/backend/src/main/java/com/yohoard/notes/NoteService.java
@@ -3,21 +3,29 @@ package com.yohoard.notes;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class NoteService {
 
+  private static final Logger LOG = LoggerFactory.getLogger(NoteService.class);
+
   @Autowired
   private NoteRepository noteRepository;
 
   public List<Note> getAllNotes() {
-    return noteRepository.findAll();
+    List<Note> notesList = noteRepository.findAll();
+    LOG.info("Sent notesList of size: {}", notesList.size());
+    return notesList;
   }
 
   public Note createNote(Note note) {
-    return noteRepository.save(note);
+    Note newNote = noteRepository.save(note);
+    LOG.info("Successfully created a note.");
+    return newNote;
   }
 
   public void deleteNote(String id) {
@@ -26,8 +34,9 @@ public class NoteService {
     if (searchedNote.isPresent()) {
       String noteId = searchedNote.get().getId();
       noteRepository.deleteById(noteId);
+      LOG.info("Successfully deleted note with id: {}", id);
     } else {
-      // LOG that note of some id dont exist
+      LOG.warn("Can't delete note of given id: {}", id);
     }
   }
 
@@ -45,8 +54,9 @@ public class NoteService {
       newNote.setPriority(dataToEdit.getPriority());
 
       noteRepository.save(newNote);
+      LOG.info("Successfully updated note with id: {}", noteId);
     } else {
-      // LOG that note of some id don't exist
+      LOG.warn("Can't update note of given id: {}", noteId);
     }
 
     return dataToEdit;
