6c5ce17cbd56fbcd4a13d187d2449a12cf802448
paxnamra <paxnamra@gmail.com>
2023-06-18 15:25:14 +0200

    added more logic and controllers for notes

--- yohoard/backend/src/main/java/com/yohoard/notes/NoteController.java
+++ yohoard/backend/src/main/java/com/yohoard/notes/NoteController.java
@@ -1,9 +1,14 @@
 package com.yohoard.notes;
 
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,9 +20,28 @@ public class NoteController {
   @Autowired
   private NoteService noteService;
 
+  @GetMapping("/list")
+  public ResponseEntity<List<Note>> listAll() {
+    var allNotes = noteService.getAllNotes();
+    return new ResponseEntity<>(allNotes, HttpStatus.OK);
+  }
+
   @PostMapping("/create")
   public ResponseEntity<Note> create(@RequestBody Note note) {
-    Note newNote = noteService.createNote(note);
+    var newNote = noteService.createNote(note);
     return new ResponseEntity<>(newNote, HttpStatus.CREATED);
+  }
+
+  @DeleteMapping("/delete/{id}")
+  public ResponseEntity<String> delete(@PathVariable("id") String id) {
+    noteService.deleteNote(id);
+    return new ResponseEntity<>(id, HttpStatus.NO_CONTENT);
+    // Change response entity to return explicit info on what note was deleted
+  }
+
+  @PutMapping("/edit/{id}")
+  public ResponseEntity<Note> update(@PathVariable("id") String id, @RequestBody Note note) {
+    var updatedNote = noteService.updateNote(id, note);
+    return new ResponseEntity<>(updatedNote, HttpStatus.OK);
   }
 }
--- yohoard/backend/src/main/java/com/yohoard/notes/NoteService.java
+++ yohoard/backend/src/main/java/com/yohoard/notes/NoteService.java
@@ -1,5 +1,8 @@
 package com.yohoard.notes;
 
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -9,7 +12,43 @@ public class NoteService {
   @Autowired
   private NoteRepository noteRepository;
 
+  public List<Note> getAllNotes() {
+    return noteRepository.findAll();
+  }
+
   public Note createNote(Note note) {
     return noteRepository.save(note);
+  }
+
+  public void deleteNote(String id) {
+    Optional<Note> searchedNote = noteRepository.findById(id);
+
+    if (searchedNote.isPresent()) {
+      String noteId = searchedNote.get().getId();
+      noteRepository.deleteById(noteId);
+    } else {
+      // LOG that note of some id dont exist
+    }
+  }
+
+  public Note updateNote(String noteId, Note dataToEdit) {
+    Optional<Note> originalNote = noteRepository.findById(noteId);
+
+    if (originalNote.isPresent()) {
+      Note newNote = originalNote.get();
+      newNote.setName(dataToEdit.getName());
+      newNote.setText(dataToEdit.getText());
+      newNote.setUpdatedAt(LocalDateTime.now());
+      newNote.setCategories(dataToEdit.getCategories());
+      newNote.setTags(dataToEdit.getTags());
+      newNote.setHighlights(dataToEdit.getHighlights());
+      newNote.setPriority(dataToEdit.getPriority());
+
+      noteRepository.save(newNote);
+    } else {
+      // LOG that note of some id don't exist
+    }
+
+    return dataToEdit;
   }
 }
